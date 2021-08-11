e16cbc02ebbd10a8abd92063ae34c467329d3843
T0NI001 <toni.ardanuy@gmail.com>
2021-08-11 22:06:36 +0200

    Tercer commit - Input, Output Services

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html'
+})
+
+export class AgregarComponent {
+
+
+  @Input() nuevo: Personaje = { nombre: "", poder: 0 };
+  //@Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+  constructor(private DbzService: DbzService) {}
+
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) { return; }
+
+    console.log(this.nuevo)
+
+    //this.onNuevoPersonaje.emit(this.nuevo);
+    this.DbzService.agregarPersonaje(this.nuevo);
+
+
+    this.nuevo = { nombre: '', poder: 0 }
+  }
+
+}
